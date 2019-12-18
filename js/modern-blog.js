5fd804eae909bc6cd04bf2c3c1860df670afd35a
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-18 12:18:25 +0000

    Some minor changes added
    Some minor changes added

--- js/modern-blog.js
+++ js/modern-blog.js
@@ -110,7 +110,7 @@ var demo = (function (window) {
 
         var elements = $(SELECTORS.card);
 
-        $.each(elements, function (card, 5-i) {
+        $.each(elements, function (card, i) {
 
             var instance = new Card(5-i, card);
 
